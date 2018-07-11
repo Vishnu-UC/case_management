b26aff02f5f84fc1e813953bf2693aea48373cdb
Vishnu S <vishnus@unicourt.com>
2018-07-11 14:24:15 +0530

    Settings changed

--- case_management/settings.py
+++ case_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p4e=uuy6mk$%^(v^5ifv_1prv(c7kj8sj7a-fiv*mmc7@udm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'courtdata'
 ]
 
 MIDDLEWARE = [
