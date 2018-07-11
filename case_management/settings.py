34a67de00935f7ec6bfb2e26fead344cd72d77e8
Vishnu S <vishnus@unicourt.com>
2018-07-11 10:43:52 +0530

    Settings changed

--- case_management/settings.py
+++ case_management/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p4(ce=uuy6mk$%^(v^5ifv_1prv(c7kj8sj7a-fiv*mmc7@udm'
+SECRET_KEY = 'p4e=uuy6mk$%^(v^5ifv_1prv(c7kj8sj7a-fiv*mmc7@udm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
